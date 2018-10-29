dd47ea1f288d02ee4808de8285141368d7796e08
pvsteja <tejapathri6@msitprogram.net>
2018-10-30 01:58:31 +0530

    "ads2_m1_a1_1: 3/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/M1/1.1 Graph/Solution.java
+++ ADS-2 Assignments/M1/1.1 Graph/Solution.java
@@ -2,50 +2,52 @@ import java.util.Scanner;
 /** . class for Solution
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String type = scan.nextLine();
-		int vertices = scan.nextInt();
-		int edges = scan.nextInt();
-		scan.nextLine();
-		LinearProbingHashST<Integer, String> hashst = new LinearProbingHashST<>();
-		String[] places = scan.nextLine().split(",");
-		for (int i = 0; i < places.length; i++) {
-			hashst.put(i, places[i]);
-		}
-		switch (type) {
-		case "List":
-			Graph graph = new Graph(vertices);
-			while (scan.hasNext()) {
-				String[] tokens = scan.nextLine().split(" ");
-				graph.addEdge(Integer.parseInt(tokens[0]
-					), Integer.parseInt(tokens[1]));
-			}
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String type = scan.nextLine();
+        int vertices = scan.nextInt();
+        int edges = scan.nextInt();
+        scan.nextLine();
+        LinearProbingHashST<Integer, String> hashst = new
+        LinearProbingHashST<>();
+        String[] places = scan.nextLine().split(",");
+        for (int i = 0; i < places.length; i++) {
+            hashst.put(i, places[i]);
+        }
+        switch (type) {
+        case "List":
+            Graph graph = new Graph(vertices);
+            while (scan.hasNext()) {
+                String[] tokens = scan.nextLine().split(" ");
+                graph.addEdge(Integer.parseInt(tokens[0]
+                    ), Integer.parseInt(tokens[1]));
+            }
 
-			System.out.println(graph.V() + " vertices, " + graph.E() + " edges");
-			if (graph.E() == 0) {
-				System.out.println("No edges");
-				break;
-			}
-			for (int i = 0; i < graph.V(); i++) {
-				StringBuffer str = new StringBuffer();
-				str.append(hashst.get(i) + ": ");
-				for (int w : graph.adj(i)) {
-					str.append(hashst.get(w) + " ");
-				}
-				System.out.println(str.toString());
-			}
-		}
-	}
+            System.out.println(
+              graph.V() + " vertices, " + graph.E() + " edges");
+            if (graph.E() == 0) {
+                System.out.println("No edges");
+                break;
+            }
+            for (int i = 0; i < graph.V(); i++) {
+                StringBuffer str = new StringBuffer();
+                str.append(hashst.get(i) + ": ");
+                for (int w : graph.adj(i)) {
+                    str.append(hashst.get(w) + " ");
+                }
+                System.out.println(str.toString());
+            }
+        }
+    }
 }
