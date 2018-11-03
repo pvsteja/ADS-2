9906bafcab3cf15636cb01563008a7e9d89731f2
pvsteja <tejapathri6@msitprogram.net>
2018-11-03 12:40:38 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/M6/ADS-2Week-1/Solution.java
+++ ADS-2 Assignments/M6/ADS-2Week-1/Solution.java
@@ -1,36 +1,53 @@
 import edu.princeton.cs.algs4.*;
 import java.util.Arrays;
+/**.
+ * Class for page rank.
+ */
 class PageRank {
-
-	private Digraph graphObj;
-
-	PageRank(Digraph digraph) {
-		this.graphObj = digraph;
-	}
+    /**.
+     * { var_description }
+     */
+    private Digraph graphObj;
+    PageRank(final Digraph digraph) {
+        this.graphObj = digraph;
+    }
 }
+/**.
+ * Class for web search.
+ */
 class WebSearch {
 }
+/**.
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		// read the first line of the input to get the number of vertices
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // read the first line of the input to get the number of vertices
 
-		int vertexCount = Integer.parseInt(StdIn.readLine());
-		// iterate count of vertices times
-		// to read the adjacency list from std input
-		// and build the graph
-		Digraph digraphObj = new Digraph(vertexCount);
-		for (int i = 0; i < vertexCount; i++) {
-			String[] edges = StdIn.readLine().split(" ");
-			for (int k = 1; k < edges.length; k++) {
-				digraphObj.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[k]));
-			}
-		}
-		System.out.println(digraphObj);
-		// Create page rank object and pass the digraphObj object to the constructor
-		PageRank pageRankObj = new PageRank(digraphObj);
-		// print the page rank object
-		// This part is only for the final test case
-		// File path to the web content
-		String file = "WebContent.txt";
-	}
+        int vertexCount = Integer.parseInt(StdIn.readLine());
+        // iterate count of vertices times
+        // to read the adjacency list from std input
+        // and build the graph
+        Digraph digraphObj = new Digraph(vertexCount);
+        for (int i = 0; i < vertexCount; i++) {
+            String[] edges = StdIn.readLine().split(" ");
+            for (int k = 1; k < edges.length; k++) {
+                digraphObj.addEdge(Integer.parseInt(
+                    edges[0]), Integer.parseInt(edges[k]));
+            }
+        }
+        System.out.println(digraphObj);
+        // Create page rank object and pass the
+        //digraphObj object to the constructor
+        PageRank pageRankObj = new PageRank(digraphObj);
+        // print the page rank object
+        // This part is only for the final test case
+        // File path to the web content
+        String file = "WebContent.txt";
+    }
 }
