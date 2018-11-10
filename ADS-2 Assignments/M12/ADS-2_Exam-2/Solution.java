206497bf1ddd7ce2eafd0931db525bb7e5bd1b4b
pvsteja <tejapathri6@msitprogram.net>
2018-11-10 12:28:55 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/M12/ADS-2_Exam-2/Solution.java
+++ ADS-2 Assignments/M12/ADS-2_Exam-2/Solution.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
 public class Solution {
-    /**
+    /**.
      * { function_description }
      *
      * @param      args  The arguments
@@ -14,11 +14,9 @@ public class Solution {
         // Parallel Edges are allowed...
         // Take the Graph input here...
         Scanner scan = new Scanner(System.in);
-
         int vertices = Integer.parseInt(scan.nextLine());
         int edges = Integer.parseInt(scan.nextLine());
         EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
-
         for (int i = 0; i < edges; i++) {
           String[] inputs = scan.nextLine().split(" ");
           int v = Integer.parseInt(inputs[0]);
@@ -27,15 +25,13 @@ public class Solution {
           Edge e = new Edge(v, w, wt);
           ewg.addEdge(e);
         }
-
         String caseToGo = scan.nextLine();
         switch (caseToGo) {
         case "Graph":
           System.out.println(ewg);
           break;
-
         case "DirectedPaths":
-            // Handle the case of DirectedPaths, where two integers are given.
+            // Handle the case of DirectedPaths,where two integers are given.
             // First is the src and second is the destination.
             // If the path exists print the distance between them.
             // Other wise print "No Path Found."
@@ -70,7 +66,6 @@ public class Solution {
                 System.out.println("No Path Found.");
             }
             break;
-
         default:
             break;
         }
