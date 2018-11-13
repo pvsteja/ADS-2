866642205075b9c9cf273245d29c9bd2825f9726
pvsteja <tejapathri6@msitprogram.net>
2018-11-13 17:47:16 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/M12/ADS-2_Exam-2/Solution.java
+++ ADS-2 Assignments/M12/ADS-2_Exam-2/Solution.java
@@ -44,7 +44,9 @@ public class Solution {
             //complexities for DijkstraUndirectedSP is ElogV.
             DijkstraUndirectedSP dusp = new DijkstraUndirectedSP(ewg, src);
             if (dusp.hasPathTo(dist)) {
-                System.out.println(dusp.distTo(dist));
+                for (Edge e: dusp.pathTo(dist) ) {
+                    System.out.println("edge");
+                }
             } else {
                 System.out.println("No Path Found.");
             }
